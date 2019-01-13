4bb117446cd6cbf33fc4fd4659107a65bea95bd3
Deziree' Teague <dez.teague@gmail.com>
2019-01-12 20:13:35 -0800

    wrote program.cs to get player names and call the game

--- Lab04_TicTacToe/Lab04_TicTacToe/Program.cs
+++ Lab04_TicTacToe/Lab04_TicTacToe/Program.cs
@@ -9,6 +9,35 @@ namespace Lab04_TicTacToe
         static void Main(string[] args)
         {
             Console.WriteLine("Let's Play Tic-Tac-Toe!");
+            Console.WriteLine("");
+            //instantiate new players
+            Player player1 = new Player();
+            Player player2 = new Player();
+
+            //assign markers "X" and "O" to the players
+            //marker is the property, players are the objects
+            player1.Marker = "X";
+            player2.Marker = "O";
+
+            Console.WriteLine("Player One, please enter your name:");
+            string name1 = Console.ReadLine();
+            //name is a property of the object, player one
+            //assign the name string, provided by the user, to that object
+            player1.Name = name1;
+
+            Console.WriteLine("Player Two, please enter your name:");
+            string name2 = Console.ReadLine();
+            //name is a property of the object, player two
+            //assign the name string, provided by the user, to that object
+            player2.Name = name2;
+
+            //instantiate the board
+            Board board = new Board();
+
+            //instantiate a new game, calling in player one and player two
+            Game game = new Game(player1, player2);
+            //run game
+            game.Play();
         }
     }
 }
