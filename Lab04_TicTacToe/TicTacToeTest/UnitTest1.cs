aecb9d9e076fdf0e9faebf30ff3220dda840210f
Deziree' Teague <dez.teague@gmail.com>
2019-01-13 18:20:25 -0800

    removed draw img

--- Lab04_TicTacToe/TicTacToeTest/UnitTest1.cs
+++ Lab04_TicTacToe/TicTacToeTest/UnitTest1.cs
@@ -1,14 +1,16 @@
 using System;
 using Xunit;
+using Lab04_TicTacToe;
+using Lab04_TicTacToe.Classes;
 
 namespace TicTacToeTest
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void GameWinnerTest()
         {
-
+            
         }
     }
 }
